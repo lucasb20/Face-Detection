fd964644de7090b857200f31b0043f873d42ad49
Lucas Rocha <lr359508@gmail.com>
2024-04-30 15:22:13 -0300

    feat: detectFaces and main created

--- app.py
+++ app.py
@@ -1,16 +1,28 @@
 
 import cv2 as cv
 from cv2 import CascadeClassifier
+import sys
 
+argv = sys.argv
+GREEN = (0, 255, 0)
 
-img = cv.imread("build/teste.jpeg")
-gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+def detectFaces(pathToImage):
+    img = cv.imread(pathToImage)
+    img = cv.resize(img, (640, 480))
+    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-detec = CascadeClassifier("data/haarcascade_frontalface_default.xml")
-faces = detec.detectMultiScale(gray, 1.1, 4)
+    haarCascade = CascadeClassifier("data/haarcascade_frontalface_default.xml")
+    faces = haarCascade.detectMultiScale(gray, 1.1, 4)
 
-for x, y, width, height in faces:
-    ret = cv.rectangle(img, (x, y), (x + width, y + height), (0, 255, 0), 2)
+    for x, y, w, h in faces:
+        cv.rectangle(img, (x, y), (x + w, y + h), GREEN, 2)
 
-cv.imshow("image", img)
-cv.waitKey(0)
+    cv.imshow(sys.argv[1], img)
+    cv.waitKey(0)
+
+if __name__ == '__main__':
+    if len(argv) != 2:
+        print(f"Use: {argv[0]} <path/to/image>")
+        exit(0)
+
+    detectFaces(argv[1])
