704e28bc0ee92c134b3c57ed31e3ec4ae2d0f85e
Lucas Rocha <lr359508@gmail.com>
2024-04-30 14:42:21 -0300

    feat: cascade classifier add

--- app.py
+++ app.py
@@ -1,8 +1,16 @@
 
 import cv2 as cv
+from cv2 import CascadeClassifier
 
 
 img = cv.imread("build/teste.jpeg")
+gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
-cv.imshow("Image", img)
-k = cv.waitKey(0)
+detec = CascadeClassifier("data/haarcascade_frontalface_default.xml")
+faces = detec.detectMultiScale(gray, 1.1, 4)
+
+for x, y, width, height in faces:
+    ret = cv.rectangle(img, (x, y), (x + width, y + height), (0, 255, 0), 2)
+
+cv.imshow("image", img)
+cv.waitKey(0)
