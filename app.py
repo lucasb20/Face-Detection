1d8ad28e7869910d461cd76dab9bb02b7296e8f7
Lucas Rocha <lr359508@gmail.com>
2024-04-30 16:16:01 -0300

    feat: image write added

--- app.py
+++ app.py
@@ -3,26 +3,33 @@ import cv2 as cv
 from cv2 import CascadeClassifier
 import sys
 
-argv = sys.argv
 GREEN = (0, 255, 0)
+DIM = (640, 480)
 
-def detectFaces(pathToImage):
+def detectFaces(pathToImage, res=None):
     img = cv.imread(pathToImage)
-    img = cv.resize(img, (640, 480))
+
+    if not res:
+        img = cv.resize(img, DIM, interpolation=cv.INTER_AREA)
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 
     haarCascade = CascadeClassifier("data/haarcascade_frontalface_default.xml")
-    faces = haarCascade.detectMultiScale(gray, 1.1, 4)
+    faces = haarCascade.detectMultiScale(gray, 1.1, 7)
 
     for x, y, w, h in faces:
         cv.rectangle(img, (x, y), (x + w, y + h), GREEN, 2)
 
-    cv.imshow(sys.argv[1], img)
-    cv.waitKey(0)
+    if res:
+        cv.imwrite(res, img)
+    else:
+        cv.imshow('Detected Faces', img)
+        cv.waitKey(0)
+        cv.destroyAllWindows()
 
 if __name__ == '__main__':
-    if len(argv) != 2:
-        print(f"Use: {argv[0]} <path/to/image>")
-        exit(0)
+    argv = sys.argv
 
-    detectFaces(argv[1])
+    if len(argv) < 2:
+        print(f"Use: {argv[0]} <path/to/image> [result filename]")
+        exit(0)
+    detectFaces(*argv[1:])
